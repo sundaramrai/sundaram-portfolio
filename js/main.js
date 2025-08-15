04febcd4e728195bed75435e41c759e2158351ee
Sundaram Rai <sundaramrai02@gmail.com>
2025-08-15 18:54:11 +0530

    refactor: remove local debounce and throttle functions from main.js

--- js/main.js
+++ js/main.js
@@ -13,25 +13,6 @@ const CONFIG = {
     }
 };
 
-const debounce = (func, delay) => {
-    let timeoutId;
-    return (...args) => {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => func(...args), delay);
-    };
-};
-
-const throttle = (func, delay) => {
-    let lastCall = 0;
-    return (...args) => {
-        const now = Date.now();
-        if (now - lastCall >= delay) {
-            lastCall = now;
-            func(...args);
-        }
-    };
-};
-
 const DOM = {
     body: document.body,
     html: document.documentElement,
